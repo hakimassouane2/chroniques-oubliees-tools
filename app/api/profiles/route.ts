900e5d203b6f5cc7f87148e6554fab84f28808c0
hakimassouane2 <hakimassouane@hotmail.com>
2023-10-01 18:45:32 +0200

    fix: deleted useless try catch preventing vercel build

--- app/api/profiles/route.ts
+++ app/api/profiles/route.ts
@@ -5,20 +5,15 @@ import { NextResponse } from "next/server";
 
 const uri =
   process.env.MONGO_URI ||
-  "mongodb+srv://adminhakim:DNzuwCet3MP8fOag@cluster0.rbyklg7.mongodb.net/";
+  "mongodb+srv://vercel-admin-user:SYeGHPvKdcucdq3B@cluster0.oqham14.mongodb.net/?retryWrites=true&w=majority";
 const dbName = "co-drs-db";
 
 export async function GET() {
   const client = new MongoClient(uri, {});
 
-  try {
-    await client.connect();
-    const database = client.db(dbName);
-    const profiles = database.collection("profiles");
-    const allProfiles = await profiles.find({}).toArray();
-    return NextResponse.json(allProfiles);
-  } catch (err) {
-    console.error(err);
-    return NextResponse.error();
-  }
+  await client.connect();
+  const database = client.db(dbName);
+  const profiles = database.collection("profiles");
+  const allProfiles = await profiles.find({}).toArray();
+  return NextResponse.json(allProfiles);
 }
