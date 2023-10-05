216fc7ee5365d1d69a5549cc8a18e14d6876ec30
hakimassouane2 <hakimassouane@hotmail.com>
2023-10-05 14:01:27 +0200

    wip: v0.5 of creatures page

--- app/creatures/[creature]/page.tsx
+++ app/creatures/[creature]/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import HeroSection from "@/components/HeroSection";
-import { Card, CardContent, Container, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useLoading } from "../../contexts/LoadingContext";
 
@@ -27,18 +34,110 @@ const CreatureDetail = ({ params }: { params: { creature: string } }) => {
         title={currentCreature?.name[0]?.label}
       />
       <Container maxWidth="xl">
-        <Typography variant="h1">{currentCreature?.name[0]?.label}</Typography>
-        <Card>
-          <CardContent>
-            <Typography>
-              Category: {currentCreature?.category[0]?.label}
-            </Typography>
-            <Typography>
-              Environment: {currentCreature?.environment[0]?.label}
-            </Typography>
-            {/* Add other stats and details here */}
-          </CardContent>
-        </Card>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={12}
+          lg={12}
+          sx={{ position: "relative", mt: -10, mb: 10 }}
+        >
+          <Card>
+            <Grid container>
+              <Grid item xs={8} sm={8} md={8} lg={8}>
+                <CardContent sx={{ p: 2 }}>
+                  <Typography
+                    variant="body2"
+                    fontFamily={"Roboto"}
+                    fontWeight={300}
+                  >
+                    NC: {currentCreature?.level[0]?.value}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    fontFamily={"Roboto"}
+                    fontWeight={300}
+                  >
+                    Famille de créature:{" "}
+                    {currentCreature?.creature_family[0]?.label}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    fontFamily={"Roboto"}
+                    fontWeight={300}
+                  >
+                    Type de boss: {currentCreature?.boss_type[0]?.label}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    fontFamily={"Roboto"}
+                    fontWeight={300}
+                    gutterBottom
+                  >
+                    Rang de boss: {currentCreature?.boss_rank[0]?.value}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    fontFamily={"Roboto"}
+                    fontWeight={300}
+                    gutterBottom
+                  >
+                    {currentCreature?.appearance[0]?.value}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    fontFamily={"Roboto"}
+                    fontWeight={300}
+                    gutterBottom
+                  >
+                    {currentCreature?.description[0]?.value}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    fontFamily={"Roboto"}
+                    fontWeight={300}
+                    gutterBottom
+                  >
+                    Réduction des dommages:{" "}
+                    {currentCreature?.dmg_reduction[0]?.value}
+                  </Typography>
+                  <Typography variant="h5" color={"primary"} gutterBottom>
+                    Attaques
+                  </Typography>
+                  {currentCreature?.attacks[0]?.data?.map((attack: any) => (
+                    <Typography
+                      variant="body2"
+                      fontFamily={"Roboto"}
+                      fontWeight={300}
+                      key={attack.name}
+                      gutterBottom
+                    >
+                      {attack.name} {attack.test} DM {attack.dm}{" "}
+                      {attack.special}
+                    </Typography>
+                  ))}
+                  <Typography variant="h5" color={"primary"} gutterBottom>
+                    Capacité(s) spéciales
+                  </Typography>
+                  <Typography variant="h5" color={"primary"} gutterBottom>
+                    Voie(s)
+                  </Typography>
+                  <Typography variant="h5" color={"primary"} gutterBottom>
+                    Capacité(s)
+                  </Typography>
+                </CardContent>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} lg={4}>
+                <CardMedia
+                  component="img"
+                  alt={"image"}
+                  image={currentCreature?.picture[0]?.creature_token_url}
+                  sx={{ backgroundColor: "white" }}
+                />
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
       </Container>
     </div>
   );
